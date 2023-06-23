b7539c41bc07ca735ff37d0066209b4487e065f4
Mikado Nakamoto <mikado.nakamoto.painestrea@gmail.com>
2023-06-23 14:42:11 +0700

    new config

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from app.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", "postgresql://postgres:!S7andard1ze@localhost:5432/StandardizeDevelopment")
+config.set_main_option("sqlalchemy.url", "postgresql://postgres:!root@localhost:5432/StandardizeDevelopment")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import router
 from app import models, database
 
 models.Base.metadata.create_all(bind=database.engine)
 
-
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(router=router)
